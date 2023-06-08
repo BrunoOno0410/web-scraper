135663d1efbbe65c38b50c82dcc0914e981f3269
Bruno Giordano Ono <69127555+BrunoOno0410@users.noreply.github.com>
2023-06-08 14:44:21 -0300

    Update script.py

--- script.py
+++ script.py
@@ -4,9 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import csv
 
 # Caminho do arquivo CSV
-csv_file = "resultados.csv"  # Arquivo criado para salvar resultados
+csv_file = "resultados.csv"
 
 # Configuração do driver do Selenium (utilizando o Chrome)
 chrome_options = webdriver.ChromeOptions()
@@ -14,13 +15,10 @@ chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(
-    "./chromedriver", options=chrome_options  # Configurar caminho do driver
-)
-# ./chromedriver padrão (executável na mesma pasta do script)
+driver = webdriver.Chrome("./chromedriver", options=chrome_options)
 
 # Acessa a página desejada
-url = "link-desejado-aqui"  # Colocar site desejado
+url = "https://csgo500.com/pt/wheel"
 
 # Definindo o tempo de execução em segundos
 tempo_execucao = 30
@@ -37,19 +35,16 @@ while True:
     soup = BeautifulSoup(html, "html.parser")
 
     # Extrai os dados desejados com base na estrutura HTML
-    class_value = results.get("class")[-1]
-    # Pegar última palavra da classe (resultado) = "black, red, blue, yellow"
+    
     results = soup.find(attrs={"role": "listitem"})
-
-    array = []
-    array.append(class_value)
-    print(array)
+    class_value = results.get("class")[-1]
+    print(class_value)
 
     # Abrir o arquivo CSV em modo de escrita
-    with open(csv_file, "w", newline="") as file:
-        writer = csv_file.writer(file)
+    with open("resultados.csv", "a", newline="") as file:
+        writer = csv.writer(file)
         # Escrever os valores no arquivo CSV
-        writer.writerow(array)
+        writer.writerow([class_value])
 
     # Aguarda 30 segundos antes da próxima iteração
     time.sleep(tempo_execucao)
