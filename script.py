5c853a576c25562390b54afc3fd48110597ab3be
Bruno Giordano Ono <69127555+BrunoOno0410@users.noreply.github.com>
2023-06-08 14:47:17 -0300

    Update script.py

--- script.py
+++ script.py
@@ -15,10 +15,13 @@ chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome("./chromedriver", options=chrome_options)
+driver = webdriver.Chrome(
+    "./chromedriver", options=chrome_options  # Configurar caminho do driver
+)
+# ./chromedriver padrão (executável na mesma pasta do script)
 
 # Acessa a página desejada
-url = "https://csgo500.com/pt/wheel"
+url = "link-desejado-aqui"  # Colocar site desejado
 
 # Definindo o tempo de execução em segundos
 tempo_execucao = 30
